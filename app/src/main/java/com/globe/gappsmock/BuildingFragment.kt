f030e785aaa68783ef60aea6e56f2490a263117f
Renz John Fronda <10015296@globetel.com>
2022-11-02 15:05:08 +0800

    no dropdown list

--- app/src/main/java/com/globe/gappsmock/BuildingFragment.kt
+++ app/src/main/java/com/globe/gappsmock/BuildingFragment.kt
@@ -30,12 +30,47 @@ class BuildingFragment : Fragment(){
         // get id of spinner
         val dropDown = view?.findViewById<Spinner>(R.id.dropFloors)
 
-        if (dropDown != null) {
+
+
+
             //get the list of string to an adapter for dropDown(Spinner)
-            val adapter = ArrayAdapter(this,
-                android.R.layout.simple_spinner_item, floors)
-            dropDown.adapter = adapter
-        }
+            val adapter = activity?.let {
+                ArrayAdapter(
+                    it,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    floors
+                )
+            }?.also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                if (dropDown != null) {
+                    dropDown.adapter = adapter
+                }
+            }
+
+
+
+
+
+
+
+            /*dropDown.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    println("helloooo")
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+                }*/
+
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_building, container, false)
